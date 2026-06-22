8c855fbb67a7a3f18d6a30b659d6bd94711d5528
Biz02m <mikolajbisewski@wp.pl>
2026-06-22 18:11:57 +0200

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import os
+
 import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+STEAM_API_KEY = os.getenv("STEAM_API_KEY")
+
+Helldivers2_ID = "553850"
+cursor = ""
+Url = f"https://store.steampowered.com/appreviews/{Helldivers2_ID}?json=1&num_per_page=100&filter=recent&{cursor}"
+
+
+Response = requests.get(Url)
+
+print(Response.content)
